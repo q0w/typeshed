7ff6cf719e6de2f8457045813d0c3946e69b0d56
q0w <43147888+q0w@users.noreply.github.com>
2022-01-19 16:03:48 +0300

    Use SupportsItemsAndItemAccess

--- stdlib/urllib/request.pyi
+++ stdlib/urllib/request.pyi
@@ -1,6 +1,6 @@
 import ssl
 import sys
-from _typeshed import StrOrBytesPath, SupportsItemsAndGetItem
+from _typeshed import StrOrBytesPath, SupportsItemsAndItemAccess
 from email.message import Message
 from http.client import HTTPMessage, HTTPResponse, _HTTPConnectionProtocol
 from http.cookiejar import CookieJar
@@ -52,8 +52,8 @@ class Request:
     origin_req_host: str
     selector: str
     data: bytes | None
-    headers: SupportsItemsAndGetItem[str, str]
-    unredirected_hdrs: SupportsItemsAndGetItem[str, str]
+    headers: SupportsItemsAndItemAccess[str, str]
+    unredirected_hdrs: SupportsItemsAndItemAccess[str, str]
     unverifiable: bool
     method: str | None
     timeout: float | None  # Undocumented, only set after __init__() by OpenerDirector.open()
@@ -61,7 +61,7 @@ class Request:
         self,
         url: str,
         data: bytes | None = ...,
-        headers: SupportsItemsAndGetItem[str, str] = ...,
+        headers: SupportsItemsAndItemAccess[str, str] = ...,
         origin_req_host: str | None = ...,
         unverifiable: bool = ...,
         method: str | None = ...,
